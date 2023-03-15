4c0a240333458cee2bb5f72f315d9862176babc1
mattiavolpe <64392380+MatthewFox90@users.noreply.github.com>
2023-03-15 13:11:06 +0100

    INIT basic HTML, CSS and shopping list creation

---
+++ assets/js/main.js
@@ -0,0 +1,13 @@
+/*
+Lista della spesa con ciclo while
+
+Consegna:
+Data una lista della spesa, stampare sulla pagina (anche brutalmente, basta che si vedano) gli elementi della lista individualmente con un ciclo while.
+
+Consigli:
+Ricordiamoci di inizializzare la variabile di contatore prima dell'inizio del ciclo while
+Ricordiamoci di incrementare il contatore, o comunque di inserire una condizione di uscita, all'interno ciclo while
+*/
+
+// CREATE THE SHOPPING LIST ARRAY
+const shoppingList = ["acqua", "pane", "farina", "birra", "mozzarella", "pasta", "bresaola", "petto di pollo", "pesce spada", "uova", "cioccolato fondente", "frollini al cacao"];
